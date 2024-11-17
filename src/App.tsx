a9347c075c4334522693b7b5917c1ed6a332e28f
ignacioesanchez1 <ignacioutnfrm@gmail.com>
2024-11-17 00:37:50 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { ComponentCounter } from "./components/ComponentCounter/ComponentCounter"
+import { MiPrimerComponent } from "./components/MiPrimerComponent/MiPrimerComponent"
+import { ComponentUseEffect } from "./components/ComponentUseEffect/ComponentUseEffect"
+import { FormComponent } from "./components/FormComponent/FormComponent"
+import { AppProduct } from "./components/AppProduct/AppProduct"
+
+
+export const App = () => {
+    
+  return (
+    <div style={{display: 'flex', flexDirection:'column', gap:"2vh"}}>
+         {
+          
+         }
+        <AppProduct/>
+         
+    </div>
+  )
+}
+
